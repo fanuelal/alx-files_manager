6cde4359791836f78f549879f7971815c5ec9c1a
Yusuf Ridwan <horiyorrmi72@gmail.com>
2022-06-12 12:51:11 +0100

    correct const varible name

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get('routes/index.j', (req, res) =>{
     res.send()
 })
 
-app.listen(port,() => {
-    console.log(`app listening on port : ${port}`);
+app.listen(PORT,() => {
+    console.log(`app listening on port : ${PORT}`);
 })
