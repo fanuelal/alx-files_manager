b0604397ac62369104587fc4e1aa2f5deec5e9dd
fanuelal <fanuelalmawstop@gmail.com>
2022-06-12 02:42:30 -0700

    mongodb with error

---
+++ utils/db.js
@@ -0,0 +1,33 @@
+const { MongoClient } = require('mongodb');
+
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${host}:${port}`;
+
+class DBClient {
+    constructor(){
+	MongoClient.connect(url, (error, client) => {
+	if(!error) this.db = client.db(database);
+	else {
+	    this.db = false;
+	}
+	});
+
+    }
+    isAlive(){
+	if (this.db) return true;
+	return false;
+    }
+    async nbUsers(){
+	const amountUsr = this.db.collection('users').countDocuments();
+	return amountUsr;
+    }
+    async nbFiles(){
+	const amountFls = this.db.collection('files').countDocuments();
+	return amountFls;
+    }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
