bab1982b8c3bd08fe5b1c18c727d478552b6d12d
fanuelal <fanuelalmawstop@gmail.com>
2022-06-12 01:40:55 -0700

    client correction

--- utils/redis.js
+++ utils/redis.js
@@ -7,22 +7,26 @@ class RedisClient{
     constructor(){
 	this.client = createClient();
 	this.getAsync = promisify(this.client.get).bind(this.client);
-	this.client.on('error', (err) => console.log(`Error on: $(err)`));
+	this.client.on('error', (err) =>{
+	    console.log(`Error on: $(err)`)
+	});
     }
 
   isAlive(){
-    return this.redis.connected;
+    return this.client.connected;
 }
 
   async get(key){
     return await this.getAsync(key);
-}
+  }
+    
   async set(key, value, time){
       this.client.set(key, value);
       this.client.expire(key, time);
   }
+    
   async del(key){
-      this.redis.del(key);
+      this.client.del(key);
   }
 }
 
