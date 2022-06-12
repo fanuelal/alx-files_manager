f8fa0dd38c531588da59a173cc2af5412a38771d
fanuelal <fanuelalmawstop@gmail.com>
2022-06-12 02:46:52 -0700

    db eslint fix

--- utils/db.js
+++ utils/db.js
@@ -6,27 +6,29 @@ const database = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${host}:${port}`;
 
 class DBClient {
-    constructor(){
-	MongoClient.connect(url, (error, client) => {
-	if(!error) this.db = client.db(database);
-	else {
-	    this.db = false;
-	}
-	});
+  constructor() {
+    MongoClient.connect(url, (error, client) => {
+      if (!error) this.db = client.db(database);
+      else {
+        this.db = false;
+      }
+    });
+  }
 
-    }
-    isAlive(){
-	if (this.db) return true;
-	return false;
-    }
-    async nbUsers(){
-	const amountUsr = this.db.collection('users').countDocuments();
-	return amountUsr;
-    }
-    async nbFiles(){
-	const amountFls = this.db.collection('files').countDocuments();
-	return amountFls;
-    }
+  isAlive() {
+    if (this.db) return true;
+    return false;
+  }
+
+  async nbUsers() {
+    const amountUsr = this.db.collection('users').countDocuments();
+    return amountUsr;
+  }
+
+  async nbFiles() {
+    const amountFls = this.db.collection('files').countDocuments();
+    return amountFls;
+  }
 }
 
 const dbClient = new DBClient();
