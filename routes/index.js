9c8443587c9da7b1909236db39c3afd996ccc32e
fanuelal <fanuelalmawstop@gmail.com>
2022-06-15 12:01:40 -0700

    redis fix

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,9 @@
-import AppController from "./controllers/AppController";
+import AppController from '../controllers/AppController';
 
-const express = require("express");
+const express = require('express');
 
-const route = (app) => {
-  const paths = express.route();
-  app.use(express.json());
-  app.use("/", paths);
-
-  paths.get("/status", (request, response) =>
-    AppController.getStatus(request, response)
-  );
-  paths.get("/stats", (request, response) =>
-    AppController.getStats(request, response)
-  );
-};
+const route = express.Router();
+route.get('/status', AppController.getStatus);
+route.get('/stats', AppController.getStatus);
 
 module.exports = route;
